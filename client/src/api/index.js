23ab29dcc6a5e8ed02cc98bcd13d4f4ee631f9ea
kel-man <kyyafuso@email.com>
2021-10-06 14:32:00 -0700

    adds api frontend connector for user

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -10,12 +10,23 @@ export const updateMovieById = (id, payload) => api.put(`/movie/${id}`, payload)
 export const deleteMovieById = id => api.delete(`/movie/${id}`)
 export const getMovieById = id => api.get(`/movie/${id}`)
 
+export const createUser = payload => api.post(`/user`, payload)
+export const indexUsers = () => api.get(`/users`)
+export const updateUserById = (id, payload) => api.put(`/user/${id}`, payload)
+export const deleteUserById = id => api.delete(`/user/${id}`)
+export const getUserById = id => api.get(`/user/${id}`)
+
 const apis = {
-    insertMovie,
-    getAllMovies,
-    updateMovieById,
-    deleteMovieById,
-    getMovieById,
+  insertMovie,
+  getAllMovies,
+  updateMovieById,
+  deleteMovieById,
+  getMovieById,
+  createUser,
+  indexUsers,
+  updateUserById,
+  deleteUserById,
+  getUserById,
 }
 
 export default apis
